a8937492786c914a2bd4209f5bbfe9218f496001
KazeSword <61825685+Jokerxp@users.noreply.github.com>
2022-10-11 23:53:54 -0400

    Final version

--- app/server.R
+++ app/server.R
@@ -58,33 +58,92 @@ df = df %>%
   mutate(region=as.character(zip_code)) %>%
   mutate(inspection_date = strptime(inspection_date,"%m/%d/%Y %H:%M:%S")) %>%
   mutate(year = year(inspection_date)) %>%
-  mutate(month = month(inspection_date))
-
+  mutate(month = month(inspection_date)) 
+  
 
 df_post_2020 = df %>% 
   filter(year(inspection_date) >= 2020) %>%
   sample_n(50000) 
+write.csv(df_post_2020, "../data/df_post_2020.csv")
 
 df_pre_2020 = df %>% 
   filter(year(inspection_date) < 2020) %>%
   sample_n(50000)
+write.csv(df_pre_2020, "../data/df_pre_2020.csv")
+
+df_pre_2020$inspection_date_month_yr=format(as.Date(df_pre_2020$inspection_date), "%Y-%m")
+
+df_post_2020$inspection_date_month_yr=format(as.Date(df_post_2020$inspection_date), "%Y-%m")
+
+if(!(file.exists("../data/Electic_post_2018.csv"))){
+  elec = read.csv("../data/Electric_Consumption_And_Cost__2010_-_Feb_2022_.csv")
+  elec_post_2018 = elec %>% 
+    set_names(tolower(names(elec))) %>%
+    filter(year(strptime(elec$revenue.month,"%Y-%M")) >= 2018) 
+  
+  write.csv(elec_post_2018, "../data/Electic_post_2018.csv")
+}
+
+
+energy = read.csv("../data/Electic_post_2018.csv")
+
+
+energy_post_2020 = energy %>% 
+  filter(year(strptime(energy$revenue.month,"%Y-%M")) >= 2020,year(strptime(energy$revenue.month,"%Y-%M")) < 2021)
+
+energy_pre_2020 = energy %>% 
+  filter(year(strptime(energy$revenue.month,"%Y-%M")) < 2020) 
+
+
+
+energy_pre_2020_group=energy_pre_2020 %>%
+  group_by(borough,revenue.month) %>%
+  summarise(sum_consumption..kwh=sum(consumption..kwh.),sum_kwh.charges=sum(kwh.charges),
+            sum_consumption..kw.=sum(consumption..kw.),
+            kw.charges = sum(kw.charges))%>%
+  filter(year(strptime(revenue.month,"%Y-%M")) < 2020) 
+
+energy_post_2020_group=energy_post_2020 %>%
+  group_by(borough,revenue.month) %>%
+  summarise(sum_consumption..kwh=sum(consumption..kwh.),sum_kwh.charges=sum(kwh.charges),sum_consumption..kw.=sum(consumption..kw.),
+            kw.charges = sum(kw.charges))
+
+
+df_pre_2020_group= df_pre_2020%>%
+  group_by(inspection_date_month_yr,borough)%>%count()
+
+df_post_2020_group= df_post_2020%>%
+  group_by(inspection_date_month_yr,borough)%>%count()
 
 
 shinyServer(function(input, output) {
   
   ## Map Tab section
   output$left_map <- renderPlot({
-    if (input$inspection_type =='Overall') {
+    if (input$inspection_type =='Overall' & input$result=="Overall") {
       leaflet_plt_df = df_pre_2020 %>% 
         group_by(region) %>%
         summarise(
           value = n()
         ) 
-    } 
-    
-    else{
+    } else if(input$inspection_type != "Overall"){
+      leaflet_plt_df = df_pre_2020 %>% 
+        filter(inspection_type == input$inspection_type) %>%
+        group_by(region) %>%
+        summarise(
+          value = n()
+        ) 
+    }else if(input$result != "Overall"){
+      leaflet_plt_df = df_pre_2020 %>% 
+        filter(result == input$result) %>%
+        group_by(region) %>%
+        summarise(
+          value = n()
+        ) 
+    }else{
       leaflet_plt_df = df_pre_2020 %>%
         filter(inspection_type == input$inspection_type) %>%
+        filter(result == input$result) %>% 
         group_by(region) %>%
         summarise(
           value = n()
@@ -97,22 +156,33 @@ shinyServer(function(input, output) {
                    legend      = "Number of inspections",
                    county_zoom = 36061)
     
-    
-    
   }) #left map plot
   
   output$right_map <- renderPlot({
-    if (input$inspection_type =='Overall') {
-      leaflet_plt_df = df_post_2020 %>% 
+    if (input$inspection_type =='Overall' & input$result=="Overall") {
+      leaflet_plt_df = df_pre_2020 %>% 
         group_by(region) %>%
         summarise(
           value = n()
         ) 
-    } 
-    
-    else{
-      leaflet_plt_df = df_post_2020 %>%
+    } else if(input$inspection_type != "Overall"){
+      leaflet_plt_df = df_pre_2020 %>% 
         filter(inspection_type == input$inspection_type) %>%
+        group_by(region) %>%
+        summarise(
+          value = n()
+        ) 
+    }else if(input$result != "Overall"){
+      leaflet_plt_df = df_pre_2020 %>% 
+        filter(result == input$result) %>%
+        group_by(region) %>%
+        summarise(
+          value = n()
+        ) 
+    }else{
+      leaflet_plt_df = df_pre_2020 %>%
+        filter(inspection_type == input$inspection_type) %>%
+        filter(result == input$result) %>% 
         group_by(region) %>%
         summarise(
           value = n()
@@ -236,5 +306,48 @@ shinyServer(function(input, output) {
         ylab("Count") +
         ggtitle("Run Chart")
     }
+  })
+  
+
+  
+  output$plotgraph1 = renderPlot({
+    tmp = df_pre_2020_group %>% 
+      filter(borough == input$borough2)
+    ggplot(tmp, aes(x =inspection_date_month_yr,y=n,group = 1)) +
+      geom_line()+
+      xlab("Year") +
+      ylab("Count") + scale_x_discrete(guide = guide_axis(angle = 90))+
+      ggtitle("Count of Rodent complains Precovid")
+  })
+  
+  output$plotgraph2 <- renderPlot({
+    tmp = df_post_2020_group %>% 
+      filter(borough == input$borough2)
+    ggplot(tmp, aes(x =inspection_date_month_yr,y=n,group = 1)) +
+      geom_line()+
+      xlab("Year") +
+      ylab("Count") + scale_x_discrete(guide = guide_axis(angle = 90))+
+      ggtitle("Count of Rodent complains Post covid")
+  })
+  
+  output$plotgraph3 <- renderPlot({
+    tmp = energy_pre_2020_group %>% 
+      filter(borough == toupper(input$borough2))
+    ggplot(tmp, aes(x =revenue.month,y=sum_consumption..kwh,group=1)) +geom_line()+
+      xlab("Year") +
+      ylab("sum_consumption..kwh") + scale_x_discrete(guide = guide_axis(angle = 90))+
+      ggtitle("Electricity Precovid")
+  })
+  
+  output$plotgraph4 <- renderPlot({
+    tmp = energy_post_2020_group %>% 
+      filter(borough == toupper(input$borough2))
+    ggplot(tmp, aes(x =revenue.month,y=sum_consumption..kwh,group=1)) +geom_line()+
+      xlab("Year") +
+      ylab("sum_consumption..kwh") + scale_x_discrete(guide = guide_axis(angle = 90))+
+      ggtitle("Electricity Postcovid")
+    
+    
+
   })
 })
--- app/ui.R
+++ app/ui.R
@@ -39,14 +39,19 @@ shinyUI(
                                       div(
                                           tags$h3("Introduction",style="text-align:center"),
                                           # Content
-                                          tags$p("Input here",style="font-size:18px")
+                                          tags$p("    Our team presents a vision for a property maintenance and extermination services company that helps residents of New York City understand the impacts of rodent infestations on energy consumption and the prevalence of rodent infestations in different regions in New York City. Rodent infestations can have many negative impacts on communities and can carry diseases, affect the safety of residents, and affect various essential utilities and processes in buildings including wiring, piping, sanitation systems, and heating systems. Thus, it is essential for residents of New York City to be aware of the levels of rodent infestations in their specific neighborhoods, the frequency of inspections conducted, and how the infestations can affect their energy costs.
+
+​​We believe that these companies should rely on data driven models for their recommendations. Using open data from New York City, we present dashboards that creatively display rodent data and correlations to energy costs to make it easier to understand for NYC residents.
+",style="font-size:18px")
                                       )
                           ),
                         column(width = 6, offset = 0,
                              div(       
                                tags$h3("Conclusion",style="text-align:center"),
                                # Content
-                               tags$p("Input here",style="font-size:18px")
+                               tags$p("In a densely populated city like New York, the presence of large numbers of rodents is a common problem. Rodent infestations can cause many problems including health hazards, spread of disease, and property damage. With the changes and fluctuations in the housing and rent markets caused by the COVID-19 pandemic, it is especially important for New York city residents to understand how their properties and residences could be affected by the presence of rodents. It is also important for consumers to understand how accurate the rodent data is by understanding how recently the inspections for rodents were conducted. 
+Therefore, as an extermination and property maintenance company, we sought to provide our consumers with data to understand rodent infestations in their neighborhoods and how it can affect their energy costs. We provided an interactive map of NYC to show the areas and neighborhoods where rodent infestations are most prevalent. Similarly, we displayed an interactive map that showed the prevalence of rodent inspections in each area. We also showed barplots of this data, and barplots and heat maps showing the correlation between rodent infestations and energy costs in the boroughs of New York City.
+",style="font-size:18px")
                              )
                           )
                         )
@@ -82,14 +87,28 @@ shinyUI(
                                           inline = TRUE, 
                                           status = "danger",
                                         ),
-                                        
+                                        awesomeRadio(
+                                          inputId = "result",
+                                          label = "Result type:", 
+                                          choices = c("Overall", 
+                                                      "Bait applied", 
+                                                      "Rat Activity",
+                                                      "Passed",
+                                                      "Failed for Other R",
+                                                      "Cleanup done",
+                                                      "Monitoring visit",
+                                                      "Stoppage done"),
+                                          inline = TRUE, 
+                                          status = "danger",
+                                        ),
+
                                         style = "opacity: 0.80"
                                         
                           ), #Panel Control - Closing
                       ) #Maps - Div closing
              ), #tabPanel maps closing
              
-             #------------------------------- tab panel - Maps ---------------------------------
+             #------------------------------- tab panel - EDA ---------------------------------
              tabPanel("Rodents Inspection EDA", 
                       icon = icon("chart-simple"),
                       fluidPage(
@@ -144,7 +163,42 @@ shinyUI(
                         )
                       )
              ),
+             #------------------------------- tab panel -  ---------------------------------
+             tabPanel("Energy",
+                      icon = icon("solar-panel"),
+                      fluidPage(
+                        titlePanel("Visualization of Pre/Post Covid Plot"),
+                        sidebarLayout(
+                          position = "left",
+                          sidebarPanel(
+                            "sidebar panel",
+                            selectInput(inputId = "borough2",
+                                        label = "Borough:",
+                                        choices = c(
+                                                    "Manhattan", 
+                                                    "Bronx",
+                                                    "Brooklyn", 
+                                                    "Queens", 
+                                                    "Staten Island"))
+                          ),
+                          mainPanel(
+                            "main panel",
+                            fluidRow(
+                              # splitLayout(
+                              style = "border: 1px solid silver:", cellWidths = c(100, 100, 100),
+                              plotOutput("plotgraph1"),
+                              plotOutput("plotgraph2"),
+                              plotOutput("plotgraph3"),
+                              plotOutput("plotgraph4")
+                              
+                              # )
+                            )
+                          )
+                        )
+                      )
+              ), # End tabPanel
              
+             #------------------------------- tab panel - Appendix ---------------------------------
              tabPanel("Appendix", 
                       icon = icon("circle-info"),
                       fluidPage( 
@@ -153,6 +207,8 @@ shinyUI(
                HTML(
                  "<h2> Data Sources </h2>
                 <h4> <p><li>NYC Rodent Inspection Data: <a href='https://data.cityofnewyork.us/Health/Rodent-Inspection/p937-wjvj'>NYC Rodent Inspection Dataset</a></li></p></h4>
+                <h4> <p><li>Electric Consumption And Cost Data: <a href='https://data.cityofnewyork.us/Housing-Development/Electric-Consumption-And-Cost-2010-Feb-2022-/jr24-e7cr'>Electric Consumption And Cost Dataset</a></li></p></h4>
+
                  "
                ),
                
